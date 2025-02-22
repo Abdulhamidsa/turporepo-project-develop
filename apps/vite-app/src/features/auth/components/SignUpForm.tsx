c55b5ff5152996b9df453d932d87e9a4b174b91c
Abdulhamidsa <alsaati.abdulhamid@gmail.com>
2025-02-22 13:02:00 +0100

    feat: integrate Zod validation schema in SignUpForm for improved form handling

--- apps/vite-app/src/features/auth/components/SignUpForm.tsx
+++ apps/vite-app/src/features/auth/components/SignUpForm.tsx
@@ -1,39 +1,38 @@
-import { SignUpFormData } from '@repo/data/types';
 import { Button } from '@repo/ui/components/ui/button';
 import { Input } from '@repo/ui/components/ui/input';
 import { showToast } from '@repo/ui/components/ui/toaster';
-import { FieldErrors, SubmitHandler, useForm } from 'react-hook-form';
+import { SignUpFormSchema, signUpResolver } from '@repo/zod';
+import { SubmitHandler, useForm } from 'react-hook-form';
 
 import { getErrorMessage } from '../../../../utils/getErrorMessage';
 import { useSignup } from '../../user/hooks/use.auth';
 import { AuthFormWrapper } from './AuthFormWrapper';
+
+// Define your Zod schema
 
 export default function SignupForm({
   setIsSignIn,
 }: {
   setIsSignIn: (value: boolean, prefill?: { email?: string }) => void;
 }) {
+  const { signup, isSubmitting } = useSignup();
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
-  } = useForm<SignUpFormData>();
+  } = useForm<SignUpFormSchema>({
+    resolver: signUpResolver,
+  });
 
-  const { signup, isSubmitting } = useSignup();
-
-  const onSubmit: SubmitHandler<SignUpFormData> = async (data) => {
+  const onSubmit: SubmitHandler<SignUpFormSchema> = async (data) => {
     try {
       await signup(data);
       showToast('You have successfully signed up.', 'success');
       setIsSignIn(true, { email: data.email });
     } catch (error) {
-      const errorMessage = getErrorMessage(error);
-      showToast(errorMessage, 'error');
+      showToast(getErrorMessage(error), 'error');
     }
   };
-
-  const password = watch('password');
 
   return (
     <AuthFormWrapper
@@ -48,56 +47,90 @@ export default function SignupForm({
       }
     >
       <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
-        {[
-          {
-            name: 'email',
-            type: 'email',
-            label: 'Email',
-            validation: { required: 'Email is required' },
-          },
-          {
-            name: 'username',
-            type: 'text',
-            label: 'Username',
-            validation: { required: 'Username is required' },
-          },
-          {
-            name: 'password',
-            type: 'password',
-            label: 'Password',
-            validation: { required: 'Password is required' },
-          },
-          {
-            name: 'confirmPassword',
-            type: 'password',
-            label: 'Repeat Password',
-            validation: {
-              required: 'Repeat Password is required',
-              validate: (value: string) => value === password || 'Passwords do not match',
-            },
-          },
-        ].map((field) => (
-          <div key={field.name} className="relative">
-            <Input
-              id={field.name}
-              type={field.type}
-              {...register(field.name as keyof SignUpFormData, field.validation)}
-              className="border-border focus:ring-primary focus:border-primary peer w-full rounded-md border px-3 py-2 focus:outline-none focus:ring-2"
-              placeholder=" " // Placeholder is empty to trigger label movement
-            />
-            <label
-              htmlFor={field.name}
-              className={`text-md text-muted-foreground peer-placeholder-shown:text-muted-foreground peer-placeholder-shown:text-md peer-focus:text-md peer-focus:text-primary peer-focus:bg-card absolute left-3 top-[-20px] px-1 transition-all peer-placeholder-shown:top-2 peer-focus:top-[-20px]`}
-            >
-              {field.label}
-            </label>
-            {errors[field.name as keyof FieldErrors<SignUpFormData>] && (
-              <p className="text-destructive-foreground mt-1 text-sm" role="alert">
-                {errors[field.name as keyof FieldErrors<SignUpFormData>]?.message}
-              </p>
-            )}
-          </div>
-        ))}
+        <div className="relative">
+          <Input
+            id="email"
+            type="email"
+            {...register('email')}
+            className="border-border focus:ring-primary focus:border-primary peer w-full rounded-md border px-3 py-2 focus:outline-none focus:ring-2"
+            placeholder=" "
+          />
+          <label
+            htmlFor="email"
+            className="text-md text-muted-foreground peer-placeholder-shown:text-muted-foreground peer-placeholder-shown:text-md peer-focus:text-md peer-focus:text-primary peer-focus:bg-card absolute left-3 top-[-20px] px-1 transition-all peer-placeholder-shown:top-2 peer-focus:top-[-20px]"
+          >
+            Email
+          </label>
+          {errors.email && (
+            <p className="text-destructive mt-1 text-sm" role="alert">
+              {errors.email.message}
+            </p>
+          )}
+        </div>
+
+        <div className="relative">
+          <Input
+            id="username"
+            type="text"
+            {...register('username')}
+            className="border-border focus:ring-primary focus:border-primary peer w-full rounded-md border px-3 py-2 focus:outline-none focus:ring-2"
+            placeholder=" "
+          />
+          <label
+            htmlFor="username"
+            className="text-md text-muted-foreground peer-placeholder-shown:text-muted-foreground peer-placeholder-shown:text-md peer-focus:text-md peer-focus:text-primary peer-focus:bg-card absolute left-3 top-[-20px] px-1 transition-all peer-placeholder-shown:top-2 peer-focus:top-[-20px]"
+          >
+            Username
+          </label>
+          {errors.username && (
+            <p className="text-destructive mt-1 text-sm" role="alert">
+              {errors.username.message}
+            </p>
+          )}
+        </div>
+
+        <div className="relative">
+          <Input
+            id="password"
+            type="password"
+            {...register('password')}
+            className="border-border focus:ring-primary focus:border-primary peer w-full rounded-md border px-3 py-2 focus:outline-none focus:ring-2"
+            placeholder=" "
+          />
+          <label
+            htmlFor="password"
+            className="text-md text-muted-foreground peer-placeholder-shown:text-muted-foreground peer-placeholder-shown:text-md peer-focus:text-md peer-focus:text-primary peer-focus:bg-card absolute left-3 top-[-20px] px-1 transition-all peer-placeholder-shown:top-2 peer-focus:top-[-20px]"
+          >
+            Password
+          </label>
+          {errors.password && (
+            <p className="text-destructive mt-1 text-sm" role="alert">
+              {errors.password.message}
+            </p>
+          )}
+        </div>
+
+        <div className="relative">
+          <Input
+            id="confirmPassword"
+            type="password"
+            {...register('confirmPassword')}
+            className="border-border focus:ring-primary focus:border-primary peer w-full rounded-md border px-3 py-2 focus:outline-none focus:ring-2"
+            placeholder=" "
+          />
+          <label
+            htmlFor="confirmPassword"
+            className="text-md text-muted-foreground peer-placeholder-shown:text-muted-foreground peer-placeholder-shown:text-md peer-focus:text-md peer-focus:text-primary peer-focus:bg-card absolute left-3 top-[-20px] px-1 transition-all peer-placeholder-shown:top-2 peer-focus:top-[-20px]"
+          >
+            Repeat Password
+          </label>
+          {errors.confirmPassword && (
+            <p className="text-destructive mt-1 text-sm" role="alert">
+              {errors.confirmPassword.message}
+            </p>
+          )}
+        </div>
+
         <Button type="submit" className="w-full" disabled={isSubmitting}>
           {isSubmitting ? 'Creating Account...' : 'Sign Up'}
         </Button>
